3ceeb6be84de5c0bfaff01ddc8cb202ba8c86971
marianaSilva-26 <mariana.silvadossantos.26@gmail.com>
2020-10-04 23:12:51 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HoraSistema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("obtendo a horaSistema!"); 
+            DateTime horaSistema = DateTime.Now; 
+            Console.WriteLine($"Hora do Sistema: {horaSistema}");
+        }
+    }
+}
